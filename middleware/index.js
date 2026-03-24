3eabd649a6afa0b90e62e33c1b918a739428d3aa
Mrkittunaik <159645431+Mrkittunaik@users.noreply.github.com>
2026-03-24 16:10:02 +0530

    Delete middleware directory

--- middleware/index.js
+++
@@ -1,8 +0,0 @@
-const auth = require('./auth');
-const upload = require('./upload');
-module.exports = {
-  adminAuth: auth.adminAuth,
-  userAuth: auth.userAuth,
-  optionalAuth: auth.optionalUserAuth || auth.optionalAuth,
-  upload
-};
